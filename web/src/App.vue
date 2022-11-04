52d69017f93b6f51fb75a63744315c14b6894126
Lukas van Driel <lukas@q42.nl>
2022-11-04 10:38:14 +0100

    vuetify3 + vue3 setup

---
+++ web/src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router'
+import herman from './assets/herman.jpeg';
+
+const drawer = ref(false);
+</script>
+
+<template>
+    <v-layout>
+      <v-app-bar
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>My files</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        :rail="drawer"
+      >
+        <v-list>
+          <v-list-item
+            :prepend-avatar="herman"
+            title="Herman Banken"
+            subtitle="herman@emmertjepaardenzaad.com"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px">
+        <RouterView />
+      </v-main>
+    </v-layout>
+</template>
+
+<style scoped>
+
+</style>
